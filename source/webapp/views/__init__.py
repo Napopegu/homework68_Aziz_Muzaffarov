4b62a7ec7d310ff7d9a0539868f5b0858fff0ba2
Napopegu <142897234+Napopegu@users.noreply.github.com>
2024-01-30 17:45:31 +0600

    init

---
+++ source/webapp/views/__init__.py
@@ -0,0 +1,2 @@
+from .article_views import ArticleView, IndexView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView, TestView
+from .comment_views import CommentCreateView, CommentUpdateView, CommentDeleteView
